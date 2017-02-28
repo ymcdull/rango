b59b80e349854ba9a35c4ff195d043b43fb69b04
Ubuntu <ubuntu@ip-172-31-9-235.eu-west-1.compute.internal>
2017-02-28 16:13:52 +0000

    C3-exercise rango/about

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner! Well done Frank!")
     
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/about/'> About</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
+#    url(r'^rango/about/', views.about, name = 'about'),
 ]
