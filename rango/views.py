183bc60739b8edcc3c130fd6a81db104f062c76c
Ubuntu <ubuntu@ip-172-31-9-235.eu-west-1.compute.internal>
2017-03-01 16:45:42 +0000

    C6 - models, templates and views

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,36 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from rango.models import Category
+
+from rango.models import Page
+
 def index(request):
     #return HttpResponse("Rango says hey there partner! Well done Frank!")
-	context_dict = {'boldmessage': "Pen, Apple, Ah, Apple-Pen!"}
+	category_list = Category.objects.order_by('-likes')[:5]
+#	context_dict = {'boldmessage': "Pen, Apple, Ah, Apple-Pen!"}
+	context_dict = {'categories' : category_list}
 	return render(request, 'rango/index.html', context = context_dict)
     
 def about(request):
   #  return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/about/'> About</a>")
 	context_dict = {'authorname': "Frank Zhu"}
 	return render(request, 'rango/about.html', context = context_dict)
+
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug= category_name_slug)
+		pages = Page.objects.filter(category = category)
+
+		context_dict['category'] = category
+		context_dict['pages'] = pages
+
+
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context = context_dict)
