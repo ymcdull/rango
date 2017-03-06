cab641004672233928b5be5b9ff3b2a090222d0a
Ubuntu <ubuntu@ip-172-31-9-235.eu-west-1.compute.internal>
2017-03-06 11:29:27 +0000

    C14 an and C15

--- rango/urls.py
+++ rango/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
 	url(r'^login/$', views.user_login, name = 'login'),
 	url(r'^restricted/$', views.restricted, name = 'restricted'),
 	url(r'^logout/$', views.user_logout, name = 'logout'),
+	url(r'^profile/$', views.profile, name = 'profile'),
+	url(r'^goto/$', views.track_url, name= 'track_url'),
+	url(r'^like_category/$', views.like_category, name='like_category'),
+	url(r'^suggest_category/$', views.suggest_category, name = 'suggest_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from rango.models import Category
 
@@ -16,6 +16,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from datetime import datetime
+
+from django.contrib.auth.models import User
 
 def index(request):
     #return HttpResponse("Rango says hey there partner! Well done Frank!")
@@ -82,8 +84,9 @@ def add_category(request):
 
 		else:
 			print(form.errors)
+	context_dict = {'form': form}
 
-	return render(request, 'rango/add_category.html', {'form': form})
+	return render(request, 'rango/add_category.html', context_dict)
 			
 @login_required
 def add_page(request, category_name_slug):
@@ -174,10 +177,73 @@ def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect('/rango/')
 
+def get_category_list(max_results=0, starts_with=''):
+	cat_list = []
+
+	if starts_with:
+		cat_list = Category.objects.filter(name__startswith=starts_with)
+	else:
+		cat_list = Category.objects.all()
+	
+	if max_results > 0:
+		if len(cat_list) > max_results:
+			cat_list = cat_list[:max_results]
+
+	return cat_list
+
+def suggest_category(request):
+	cat_list = []
+	starts_with = ''
+	if request.method == 'GET':
+		starts_with = request.GET['suggestion']
+		cat_list = get_category_list(8, starts_with)
+	context_dict = {'cat_list': cat_list}
+	return render(request, 'rango/category_list.html', context_dict)
+	
+@login_required
+def profile(request):
+	u = User.objects.get(username = request.user)
+
+	try:
+		up = Userprofile.objects.get(user = u)
+	except:
+		up = None
+
+	context_dict["user"] = u
+	context_dict["userprofile"] = up
+
+	return render(request, 'rango/profile.html', context_dict)
 
 
+def track_url(request):
+	page_id = None
+	url = '/rango/'
+	if request.method == "GET":
+		if 'page_id' in request.GET:
+			page_id = request.GET["page_id"]
+			try:
+				page = Page.objects.get(id=page_id)
+				page.views = page.views + 1
+				page.save()
+				url = page.url
 
+			except:
+				pass
 
+	return redirect(url)
 
+@login_required
+def like_category(request):
+	cat_id = None
+	if request.method == 'GET':
+		cat_id = request.GET['category_id']
 
+	likes = 0
+	if cat_id:
+		category = Category.objects.get(id=int(cat_id))
+		if category:
+			likes = category.likes + 1
+			category.likes = likes
+			category.save()
 
+	return HttpResponse(likes)
